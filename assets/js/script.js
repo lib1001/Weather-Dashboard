954fbefc93ca39b22423c17dba3ffde1593061b3
Lauren <lburke522@gmail.com>
2022-08-26 21:37:56 -0400

    added HTML and framework for js

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// var apiUrl = 
+var userSearchArr = [];
+
+// function to show search history on click with event listeners
+    // function to store to local storage (prevent default bc form)
+    // function to display
+    // loop through array
+
+// function to make search history results buttons to display that city data on click
+
+// call API
+
+// append current weather & icons to HTML
+// same for 5 day
+
+// call UV Index api?/
+    // function getUVIndex(lat, long) {}
+    // look at notebook
+
+// OPTIONAL?
+    // call getWeather function for current city, otherwise blank ugly screen fuck it
+    // check if in degrees F
+    // error if no city or typo or whatevs
